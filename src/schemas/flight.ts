7dc0e313fc7a879694054d9c10b28f46589a15c5
Toon Derkoningen <toon@derkoningen.com>
2025-03-05 10:19:10 +0100

    Add openapi metadata to Flight schema

--- src/schemas/flight.ts
+++ src/schemas/flight.ts
@@ -2,14 +2,32 @@ import { z } from '@hono/zod-openapi';
 
 export const Flight = z
     .object({
-        aircraft: z.string().min(1).max(10),
-        flightNumber: z.string().min(1).max(10),
-        schedule: z.object({
-            std: z.date(),
-            sta: z.date(),
+        aircraft: z.string().min(1).max(10).openapi({
+            example: 'CSTRC',
+            description: 'A code describing the aircraft assigned to the flight',
         }),
-        departure: z.string().length(4),
-        destination: z.string().length(4),
+        flightNumber: z.string().min(1).max(10).openapi({
+            example: 'AVIO201',
+            description: 'A code that identifies the flight',
+        }),
+        schedule: z.object({
+            std: z.date().openapi({
+                description: 'The scheduled time of departure, ISO 8601 format',
+                format: 'date-time',
+            }),
+            sta: z.date().openapi({
+                description: 'The scheduled time of arrival, ISO 8601 format',
+                format: 'date-time',
+            }),
+        }),
+        departure: z.string().length(4).openapi({
+            description: 'Identifier for the departure airport',
+            example: 'LPPD',
+        }),
+        destination: z.string().length(4).openapi({
+            description: 'Identifier for the destination airport',
+            example: 'LPLA',
+        }),
     })
     .openapi('Flight')
     ;
